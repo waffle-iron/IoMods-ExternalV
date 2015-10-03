cdc10568f6c4b3bfee62a6f04d6fa403d597e8cc
timothyfrancis <timothyfrancis.marano@gmail.com>
2015-10-03 08:44:04 +0000

    Create Agarmods.js

---
+++ Agarmods.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        AgarMods
+// @namespace	 AgarMods
+// @version      1.9.9
+// @description  community run mod feature set for agar.io
+// @author       electronoob
+// @updateURL    http://agariomods.com/mods.user.js
+// @match        http://agar.io/
+// @match        http://agar.io/#*
+// @match        http://agar.io/?*
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.src = document.location.protocol+"http://www.agarmods.sitew.org/#Page_1.A";
+(document.body || document.head || document.documentElement).appendChild(script);
+
+/*
+repo:
+https://github.com//agarmods
+
+http://www.agarmods.sitew.org/#Page_1.A
+
+*/
+
