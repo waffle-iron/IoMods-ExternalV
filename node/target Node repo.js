fee9b28ef65a04e069f08b6acc9f86582796aa47
timothyfrancis <timothyfrancis.marano@gmail.com>
2015-12-27 02:46:44 +0000

    Create target Node repo.js

---
+++ node/target Node repo.js
@@ -0,0 +1,15 @@
+/*
+
+repo:
+http://node.org
+
+http://github/nodejs.org/node
+
+http://github/nodejs.grg
+
+http://github.com/nodejs.org/nodejs/
+
+*/
+var repo = nodejs.org,
+var name = node,
+document.body || document.createElement || .createElement (dp || do true || do false || document.body).appendChild('script');
