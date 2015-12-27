8c99f3ff0174a91b919c54510b78de33a6f182d5
timothyfrancis <timothyfrancis.marano@gmail.com>
2015-12-27 02:52:00 +0000

    Update target Node repo.js

--- node/target Node repo.js
+++ node/target Node repo.js
@@ -13,3 +13,6 @@ http://github.com/nodejs.org/nodejs/
 var repo = nodejs.org,
 var name = node,
 document.body || document.createElement || .createElement (dp || do true || do false || document.body).appendChild('script');
+document ('script')
+var script = document || .body || .createElement,
+
