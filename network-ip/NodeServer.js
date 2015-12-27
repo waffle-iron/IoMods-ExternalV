b4603832e4dfeb881496ab068272e837d7a4767b
timothyfrancis <timothyfrancis.marano@gmail.com>
2015-12-27 23:30:12 +0000

    Update NodeServer.js

--- network-ip/NodeServer.js
+++ network-ip/NodeServer.js
@@ -2,4 +2,19 @@ var mdns = require('mdns')
 
 var ad = mdns.createAdvertisement(mdns.tcp('http'),3000,{name : 'Node Server'}
 
-ad.start();
+ad.start(5
+ad : true,
+)
+
+server.start[server.properties] {
+var mdns = ('https')online,3000,
+server.properties = mdns('script')
+var server.properties = mineos-node || Server = on,
+
+
+
+
+
+
+
+}
