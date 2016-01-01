0a06ebf389c4a9bd198c27e13d4107e6b50b0d0e
timothyfrancis <timothyfrancis.marano@gmail.com>
2016-01-01 22:47:35 +0000

    JsAddon!

---
+++ Add-on-V3.1/ElectricEvergreen/eEvergreen.js
@@ -0,0 +1,12 @@
+var eEvergreenAddonVersion = 3.1,3.2;
+
+var a = ElectricEvergreenAddon;
+
+var a = function Evergreen [a] (
+//TODO : REPO
+/*
+The Repo Addon Evergreen
+Until AgarMods Are While Starting A Webpage
+*/
+get.addonrepo true = eEvergreen;
+);
