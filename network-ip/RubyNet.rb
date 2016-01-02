198b534033c01864128fdcc3247b9e56e5827039
timothyfrancis <timothyfrancis.marano@gmail.com>
2016-01-02 23:47:21 +0000

    Create RubyNet.rb

---
+++ network-ip/RubyNet.rb
@@ -0,0 +1,5 @@
+SendMsgEmail >>>
+From: AgarSupport <timothyfrancis.marano@gmail.com>
+To: Everyone <user@AgarSupport.com>
+Msg "WELCOME AGAR SUPPORT PLS DO THIS GOTO WWW.GITHUB.COM/timothyfrancis/AgarMods-ExternalV/"
+<h2>Join Us!</h2>
