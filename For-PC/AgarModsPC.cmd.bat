b456f103c7f5b92469406b12d6d7a3535b9d7f8c
timothyfrancis <timothyfrancis.marano@gmail.com>
2016-02-15 01:23:46 +0000

    Update AgarModsPC.cmd.bat

--- For-PC/AgarModsPC.cmd.bat
+++ For-PC/AgarModsPC.cmd.bat
@@ -1,1 +1,18 @@
+Echo off
+Echo. Repo Is Installing
+ping localhost -n 5 > nul
+cls
+Echo. Repo Is installed
 rem agarMods
+set /a repo=https://www.github.com/timothyfrancis/AgarMods-ExternalV/
+cls
+ping agar.io (agar.io %agarMods%
+set label=765
+
+if label ==765 (
+goto Installed
+)else(
+exit)
+
+:Installed
+start label.js
