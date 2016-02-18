99f56a3ed808c107bd06094eab38401a9e8a9bda
Timothy Francis P. Maraño <timothyfrancis.marano@gmail.com>
2016-02-18 23:37:48 +0000

    Update Update.bat

--- For-PC/Update.bat
+++ For-PC/Update.bat
@@ -1,3 +1,6 @@
+:By Tinoy
+@echo off
+rem Level Update This Repo
 :Home
 xcopy /x /z 'https://github.com/timothyfrancis/AgarMods-ExternalV/'
 echo. Did You Want Accept LICENCE MBIG
@@ -5,3 +8,12 @@ set /p ch=Chosse Yes/No:
 if %ch%==Yes goto Yes
 if %ch%==No exit
 :Yes
+cls
+echo %temp% >> '%temp%\temp.txt'
+echo Temp Seted
+rem match:     agar.io
+echo updateCompleted
+ping localhost -n 3 >nul
+cls
+set digit=Update%3 %ToUpdate3 /n -n node
+ping agar.io {agar.io target to bad.com in hosting}
