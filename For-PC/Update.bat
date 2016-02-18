631f2dccc07b38b0cd3aae1fddf15126d01e409d
Timothy Francis P. Maraño <timothyfrancis.marano@gmail.com>
2016-02-18 23:24:40 +0000

    Update Update.bat

--- For-PC/Update.bat
+++ For-PC/Update.bat
@@ -1,2 +1,7 @@
+:Home
 xcopy /x /z 'https://github.com/timothyfrancis/AgarMods-ExternalV/'
-
+echo. Did You Want Accept LICENCE MBIG
+set /p ch=Chosse Yes/No:
+if %ch%==Yes goto Yes
+if %ch%==No exit
+:Yes
